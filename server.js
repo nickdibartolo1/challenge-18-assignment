78028fac7956c6659f2db1cb7d0c70a75e964fa8
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2022-10-31 14:20:47 -0400

    package installs

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ APP.use(express.json());
 APP.use(express.urlencoded({ extended: true }));
 APP.use(express.static('public'))
 
-APP.use(require('./routes'));
+// APP.use(require('./routes'));
 
-mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost/social-network-API',
-{
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost/social-network-API',
+// {
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
 
-mongoose.set('debug', true);
+// mongoose.set('debug', true);
 
 
-app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
+APP.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
