0cf9e19e2ef5c2db8610a7cfb9db4fe33837d934
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2022-11-01 15:09:57 -0400

    added index and user.js to model folder

--- models/thought.js
+++ models/thought.js
@@ -11,7 +11,7 @@ const reactionSchema = new Schema(
         reactionBody: {
             type: String,
             required: true,
-            maxlength: 250
+            maxlength: 280
         },
         username: {
             type: String,
@@ -38,7 +38,7 @@ const thoughtSchema = new Schema({
         type: String,
         required: true,
         minlength: 1,
-        maxlength: 250
+        maxlength: 280
     },
     createdAt: {
         type: Date,
---
+++ models/user.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const user = model('User', userSchema);
+
+// export the User model
+module.exports = user;
